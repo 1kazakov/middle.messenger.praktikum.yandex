e7e782c2f9692a03ea774d896736025bf890aa9a
Anton Kazakov <as.kazakov@list.ru>
2021-07-10 08:46:19 +0500

    add change-user-data page

---
+++ src/pages/change-user-data/change-user-data.js
@@ -0,0 +1,24 @@
+import loginTemplates from './change-user-data.temp';
+import Templator from '../../utils/templator';
+
+const tmpl = new Templator(loginTemplates);
+
+const context = {
+  namePage: 'Изменение данных пользователя',
+  avatar: {
+    image: 'какой-то url',
+  },
+  userData: [
+    { fildName: 'Почта', name: 'email', value: 'kazakov@yandex.ru' },
+    { fildName: 'Логин', name: 'login', value: 'kazakov' },
+    { fildName: 'Имя', name: 'first_name', value: 'Антон' },
+    { fildName: 'Фамилия', name: 'second_name', value: 'Казаков' },
+    { fildName: 'Имя в чате', name: 'display_name', value: 'Антон' },
+    { fildName: 'Телефон', name: 'phone', phone: '+7 (912) 646 59 00'},
+  ],
+  buttonText: 'Сохранить',
+};
+
+const renderedTemplate = tmpl.compile(context); // Строка с html-вёрсткой
+
+document.body.innerHTML = renderedTemplate; // Показался нужный результат
