cf17727cc7d5f5fcba04334b10b4155022f36056
Anton Kazakov <as.kazakov@list.ru>
2021-07-28 22:58:19 +0500

    add ts on changeUserData page

--- src/pages/change-user-data/change-user-data.temp.ts
+++ src/pages/change-user-data/change-user-data.temp.ts
@@ -4,19 +4,18 @@ export default `
     <h1 class="page__title visually-hidden"> {{ namePage }} </h1>
     <section class="page__avatar avatar">
       <div class="avatar__image-wrapper">
-        <img src="{{ avatar.image }}" class="avatar__image">
+        <img src="{{ avatar }}" class="avatar__image">
       </div>
     </section>
     <form class="page__form">
       <ul class="page__input-list input-list">
         {{ for userData }}
           <li class="input-list__item">
-            <label class="input-list__item-label" for="{{ name }}">{{ fildName }}</label>
-            <input name="{{ name }}" class="input-list__item-input" type="text" id="{{ name }}" placeholder="{{ fildName }}" value="{{ value }}"/>
+            {{ input }}
           </li>
         {{ /for }}
       </ul>
-      <button type="submit" class="input-list__button button button-primary">{{ buttonText}}</button>
+      {{ buttonSave}}
     <form>
   </main>
 </div>
