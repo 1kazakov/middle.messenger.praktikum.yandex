bb5f2103c370520892b2e901e831329307d27370
Anton Kazakov <as.kazakov@list.ru>
2021-07-28 22:32:34 +0500

    add ts on login page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,58 @@
+import pageTemplates from './login.temp';
+import Block from '../../utils/Block';
+import Input from '../../components/input/input';
+import Button from '../../components/button/button';
+
+
+const context: {
+  namePage: string;
+  login: any;
+  password: any;
+  button: any;
+  signUpText: string;
+} = {
+  namePage: 'Вход',
+  login: new Input({ fildTitle: 'Логин',
+                     name: 'login',
+                     inputClass: 'input-list__item-input',
+                     labelClass: 'input-list__item-label' }),
+  password: new Input({ fildTitle: 'Пароль',
+                        name: 'password',
+                        inputClass: 'input-list__item-input',
+                        labelClass: 'input-list__item-label' }),
+  button: new Button({ buttonName: 'Авторизоваться',
+               buttonType: 'submit',
+               buttonClass: 'input-list__button button button-primary' }),
+  signUpText: 'Создать новый аккаунт',
+};
+
+class PageLogin extends Block {
+  constructor(props: {[key: string]: any}) {
+    super('div', props, pageTemplates);
+  }
+  render() {
+    return this.templator().compile(pageTemplates, {
+      namePage: this.props.namePage,
+      signUpText: this.props.signUpText,
+      login: this.props.login.render(),
+      password: this.props.password.render(),
+      button: this.props.button.render(),
+    })
+  }
+}
+
+const page = new PageLogin(context);
+
+function render(block) {
+  document.body.appendChild(block.getContent());
+}
+
+render(page);
+
+// Через секунду контент изменится сам, достаточно обновить пропсы
+setTimeout(() => {
+  page.setProps({
+    button: 'Click me, please',
+  });
+  render(page);
+}, 1000);
