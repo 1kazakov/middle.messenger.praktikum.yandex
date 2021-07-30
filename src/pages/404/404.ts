ef077d956355af09b45efd8216d761614c0dd2b2
Anton Kazakov <as.kazakov@list.ru>
2021-07-30 16:41:45 +0500

    add ts on 404Page

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,5 +1,5 @@
 import pageTemplates from './404.temp';
-import Block from '../../utils/Block';
+import Block from '../../utils/block';
 
 const context = {
   namePage: '404',
@@ -8,24 +8,19 @@ const context = {
 };
 
 class Page404 extends Block {
-  templator: () => {
-    compile(template: string, context: {[key: string]: any}): string,
-  };
   constructor(props: {[key: string]: any}) {
     super('div', props, pageTemplates);
   }
   render() {
     return this.templator().compile(pageTemplates, {
       ...this.props,
-      button: this.props.button.render()
     })
   }
 } 
 
 const page = new Page404(context);
 
-function render(block) {
-  console.log('block.getContent()', block.getContent())
+function render(block: any) {
   document.body.appendChild(block.getContent());
 }
 
