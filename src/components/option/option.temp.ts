60efadbe0dba329621327a3036e4513a73ff5ccf
Anton Kazakov <as.kazakov@list.ru>
2021-07-31 18:37:51 +0500

    add component option

---
+++ src/components/option/option.temp.ts
@@ -0,0 +1,11 @@
+const chatTmpl: string = `
+<li class="options-list__item option">
+  <button class="option__button button-round">
+  </button>
+  <p class="option__text">
+    {{ optionText }}
+  </p>
+</li>
+`;
+
+export default chatTmpl;
