1d4d3cd3ecf546dd7aec49cab51a7b3bda2a752f
Anton Kazakov <as.kazakov@list.ru>
2021-08-01 13:27:19 +0500

    delete comment

--- src/pages/login/login.temp.ts
+++ src/pages/login/login.temp.ts
@@ -17,24 +17,3 @@ export default `
   </main>
 </div>
 `;
-
-
-// export default `
-// <div class="container">
-//   <main class="page">
-//     <h1 class="page__title"> {{ namePage }} </h1>
-//     <form class="page__form">
-//       <ul class="page-__input-list input-list">
-//         {{ for form }}
-//           <li class="input-list__item">
-//             <label class="input-list__item-label" for="user-name">{{ fildTitle }}</label>
-//             <input name="{{ name }}" class="input-list__item-input" type="text" id="user-name" placeholder="{{ fildTitle }}"/>
-//           </li>
-//         {{ /for }}
-//       </ul>
-//       <button type="submit" class="input-list__button button button-primary">{{ buttonText}}</button>
-//     <form>
-//     <a class="page__link" href="../signup/signup.html">{{ signUpText }}</a>
-//   </main>
-// </div>
-// `;
