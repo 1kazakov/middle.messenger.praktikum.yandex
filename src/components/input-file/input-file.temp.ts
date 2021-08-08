8f985b6488b303c5c83bd480f1310725e5d49bfd
Anton Kazakov <as.kazakov@list.ru>
2021-08-08 21:46:22 +0500

    fix template components

--- src/components/input-file/input-file.temp.ts
+++ src/components/input-file/input-file.temp.ts
@@ -1,10 +1,10 @@
-const inputTemp: string = `</>
-<label class="{{ labelClass }} label-input-file" for="{{ name }}">
-  <span class="input-file__name-label">{{ fildTitle }}</span>
-  <span class="{{ buttonClass }}">{{ buttonTitle }}</span>
-</label>
-<input name="{{ name }}" class="visually-hidden
-" type="file" id="{{ name }}" placeholder="{{ placeholder }}" value="{{ value }}"/>
-</>`;
+const inputTemp: string = `
+<div class="input-list__item">
+  <label class="{{ labelClass }} label-input-file" for="{{ name }}">
+    <span class="input-file__name-label">{{ fildTitle }}</span>
+    <span class="{{ buttonClass }}">{{ buttonTitle }}</span>
+  </label>
+  <input name="{{ name }}" class="visually-hidden" type="file" id="{{ name }}" placeholder="{{ placeholder }}" value="{{ value }}"/>
+<div>`;
 
 export default inputTemp;
--- src/components/message/message.temp.ts
+++ src/components/message/message.temp.ts
@@ -1,11 +1,11 @@
 const chatTmpl: string = `
 <li class="list-messages__item message {{ messageClass }}">
-<p class="message__text">
-  {{ message }}
-</p>
-<p class="message__date">
-  {{ messageDate }}
-</p>
+  <p class="message__text">
+    {{ message }}
+  </p>
+  <p class="message__date">
+    {{ messageDate }}
+  </p>
 </li>
 `;
 
