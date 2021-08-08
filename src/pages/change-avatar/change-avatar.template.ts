980160c34376848af362259eaad6b50dd6b4ab11
Anton Kazakov <as.kazakov@list.ru>
2021-08-08 20:13:31 +0500

    add new templater on change-avatar Page

--- src/pages/change-avatar/change-avatar.template.ts
+++ src/pages/change-avatar/change-avatar.template.ts
@@ -1,22 +1,24 @@
 export default `
 <div class="container">
   <main class="page">
-    <h1 class="page__title visually-hidden"> {{ namePage }} </h1>
+    <h1 class="page__title visually-hidden">
+      {{ namePage }}
+    </h1>
     <section class="page__avatar avatar">
       <div class="avatar__image-wrapper">
-        <img src="{{ avatar.image }}" class="avatar__image">
+        <img src="{{ avatar.image }}" class="avatar__image" />
       </div>
     </section>
     <form class="page__form">
       <ul class="page__input-list input-list">
-        {{ for userData }}
-          <li class="input-list__item">
+        {% userData %}
+          <li>
             {{ input }}
           </li>
-        {{ /for }}
+        {% end %}
       </ul>
       {{ buttonSave }}
-    <form>
+    </form>
   </main>
 </div>
 `;
