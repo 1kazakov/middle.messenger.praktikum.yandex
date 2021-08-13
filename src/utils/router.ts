b38530e52eb371a37fd7ee846d6e471a1f5d6433
Anton Kazakov <as.kazakov@list.ru>
2021-08-13 21:51:32 +0500

    add router

---
+++ src/utils/router.ts
@@ -0,0 +1,75 @@
+import Route from "./route"
+
+export default class Router {
+  private static __instance: Router;
+  routes: any[]
+  history: any
+  _currentRoute: any
+  _rootQuery: string
+  _blockProps: any
+
+    constructor(rootQuery: any = '.app') {
+        if (Router.__instance) {
+            return Router.__instance;
+        }
+
+        this.routes = [];
+        this.history = window.history;
+        this._currentRoute = null;
+        this._rootQuery = rootQuery;
+
+        Router.__instance = this;
+    }
+
+    use(pathname: string, block: any, props: any) {
+        const route = new Route(pathname, block, {rootQuery: this._rootQuery, props});
+        this.routes.push(route);
+        // console.log('this.routes', this.routes)
+      return this;
+    }
+
+    start() {
+    // Реагируем на изменения в адресной строке и вызываем перерисовку
+    // console.log('211111111')
+    window.onpopstate = (event: any) => {
+      event.preventDefault();
+      // console.log('onpopstate')
+      this._onRoute(event.currentTarget.location.pathname);
+    };
+
+    this._onRoute(window.location.pathname);
+    }
+
+    _onRoute(pathname: string) {
+      // console.log('this.routes', this.routes)
+      const route = this.getRoute(pathname);
+      // console.log(route)
+      // console.log(pathname)
+
+      if (this._currentRoute) {
+          this._currentRoute.leave();
+      }
+
+      this._currentRoute = route;
+      route.render(route, pathname);
+    }
+
+    go(pathname: string) {
+      console.log('99999')
+      this.history.pushState({}, "", pathname);
+      this._onRoute(pathname);
+    }
+
+    back() {
+      this.history.back();
+    }
+
+    forward() {
+      this.history.forward();
+    }
+
+    getRoute(pathname: string): any {
+        return this.routes.find(route => route.match(pathname));
+    }
+}
+
