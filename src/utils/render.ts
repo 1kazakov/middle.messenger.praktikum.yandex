3ae56b7adab0ba80445db833b0f48ea0c2d88cd5
Anton Kazakov <as.kazakov@list.ru>
2021-08-30 10:31:02 +0500

    change from render to getContent on render func

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,5 +1,4 @@
 export default function render(query: string, block: any) {
-    const root = document.querySelector(query);
-    root.append(block.render());
-    return root;
-  }
+  const root = document.querySelector(query);
+  root.append(block.getContent());
+}
