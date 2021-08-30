985703dc2971a9b91c06c6bc1db526c7afbb573f
Anton Kazakov <as.kazakov@list.ru>
2021-08-30 10:59:04 +0500

    add global eventBus

---
+++ src/utils/global-event-bus.ts
@@ -0,0 +1,12 @@
+import EventBus from "./EventBus";
+
+export default class GlobalEventBus extends EventBus {
+  private static __instance: GlobalEventBus;
+  constructor() {
+    super()
+    if (GlobalEventBus.__instance) {
+      return GlobalEventBus.__instance;
+    }
+    GlobalEventBus.__instance = this;
+  }
+}
