669e27948181f813bdf1986d48bbd8b52beb1ba6
Anton Kazakov <as.kazakov@list.ru>
2021-08-30 11:00:19 +0500

    add util events

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,5 +1,5 @@
-export default (dataAttr: string, eventName: string, event: (evt: any) => void, action: string = 'addEventListener') => {
-  const elements: any[] = [...document.querySelectorAll(`#${dataAttr}`)];
+export default (className: string, eventName: string, event: (evt: any) => void, action: string = 'addEventListener') => {
+  const elements: any[] = [...document.querySelectorAll(`.${className}`)];
   console.log(elements)
   if (elements.length) {
     elements.forEach(element => element[action](eventName, event))
