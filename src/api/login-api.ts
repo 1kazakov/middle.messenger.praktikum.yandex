d60bda2e93a1d9f4f5c17190bdda1ab4973fe11c
Anton Kazakov <as.kazakov@list.ru>
2021-09-01 23:11:52 +0500

    add baseUrl and delete baseApiClass

--- src/api/login-api.ts
+++ src/api/login-api.ts
@@ -1,16 +1,15 @@
 import HTTP from '../utils/fetch';
-import { BaseAPI } from './base-api';
 
-const authAPIInstance = new HTTP();
+const loginAPIInstance = new HTTP();
 
-export default class LoginAPI extends BaseAPI {
+export default class LoginAPI {
   public async logining(data: {[key: string]: string}) {
-    return authAPIInstance.post('https://ya-praktikum.tech/api/v2/auth/signin', data);
+    return loginAPIInstance.post('/auth/signin', data);
   }
   public async getUserData() {
-    return authAPIInstance.get('https://ya-praktikum.tech/api/v2/auth/user');
+    return loginAPIInstance.get('/auth/user');
   }
   public async logout() {
-    return authAPIInstance.post('https://ya-praktikum.tech/api/v2/auth/logout');
+    return loginAPIInstance.post('/auth/logout');
   }
 }
