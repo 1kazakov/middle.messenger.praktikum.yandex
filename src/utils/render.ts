dca12719ef1998d3824dca13edfe5ec590c1b307
Anton Kazakov <as.kazakov@list.ru>
2021-10-18 22:36:08 +0500

    add logs

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,4 +1,6 @@
 export default function render(query: string, block: any) {
   const root = document.querySelector(query);
+  console.log('root', root);
+  console.log('query', query);
   root.append(block.getContent());
 }
