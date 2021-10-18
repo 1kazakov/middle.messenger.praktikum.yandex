530dc98a53aec634cedfab25e11caa58c6340ee9
Anton Kazakov <as.kazakov@list.ru>
2021-10-18 22:39:44 +0500

    delete logs

--- src/utils/render.ts
+++ src/utils/render.ts
@@ -1,6 +1,4 @@
 export default function render(query: string, block: any) {
   const root = document.querySelector(query);
-  console.log('root', root);
-  console.log('query', query);
   root.append(block.getContent());
 }
